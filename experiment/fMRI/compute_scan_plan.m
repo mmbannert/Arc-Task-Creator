6f23184c5e5a981c0839fab2aae8c2dd3a914281
Yavuz Karaca <yavkaraca@gmail.com>
2026-06-13 05:18:46 +0200

    compute_scan_plan added

---
+++ experiment/fMRI/compute_scan_plan.m
@@ -0,0 +1,21 @@
+function scanPlan = compute_scan_plan(session)
+
+cfg = utilities.session.default_config();
+
+    sessionText = fileread(session);
+        session = jsondecode(sessionText);
+
+    experimentDuration = ...
+        session.number_of_trials_total * cfg.response_time_window + ... % all trials
+        session.number_of_blocks * 2 * cfg.rest_time; % all fixation/rest
+
+    totalDuration = cfg.dummy_seconds + experimentDuration + cfg.rest_time;
+
+    scanPlan.experiment_duration = experimentDuration;
+    scanPlan.total_duration = totalDuration;
+    scanPlan.n_TRs = ceil(totalDuration / cfg.TR);
+    scanPlan.TR = cfg.TR;
+
+    fprintf('Required scan duration: %.1f seconds\n', scanPlan.total_duration);
+    fprintf('Required TRs/volumes: %d\n', scanPlan.n_TRs);
+end

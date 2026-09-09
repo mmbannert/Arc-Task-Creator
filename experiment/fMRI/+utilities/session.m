e650df9f21178738142b16d7dabf8affc1d970a5
Yavuz Karaca <yavkaraca@gmail.com>
2026-09-09 16:17:54 +0200

    Changes merged from dev, unused rules removed

--- experiment/fMRI/+utilities/session.m
+++ experiment/fMRI/+utilities/session.m
@@ -7,31 +7,32 @@ function config = default_config()
 
     % ---- general config ----
     config.skip_sync_tests = 0; % 0 in production
-    config.rest_time = 1; % 10 in production
-    config.response_time_window = 15; % 10 in production
+    config.rest_time = 10; % 10 in production
+    config.response_time_window = 10; % 10 in production
 
     % ---- screen config ----
-    config.use_windowed_mode = true;  % false in production
+    config.use_windowed_mode = false;  % false in production
     config.window_rect = [50 50 1350 1350]; % [X_start Y_start X_end Y_end]
     config.resolution = [1300 1300];
     config.bg_color = [25 25 25]; % for grayish background
     
     % Native resolution of display device
-    % config.native_resolution = [1920, 1080];     % in 3T lab
+    config.native_resolution = [1920, 1080];     % in 3T lab
     % config.native_resolution = [1600, 1200];    % at desktop
-    config.native_resolution = [3440, 1440];    % at brunsstr
+    % config.native_resolution = [3440, 1440];    % at brunsstr
+
 
     % ---- response keys ----
-    config.keys.left      = '3#'; % index finger.
-    config.keys.right     = '4$'; % middle finger.
+    config.keys.left      = '4$'; % index finger.
+    config.keys.right     = '3#'; % middle finger.
     % 3T lab keyboard for RIGHT hand has '4$' on index finger (left-most button) and '3#' on middle finger (second-from-left button).
     % For standard keyboard swap them.
 
     % ---- EyeLink config ----
-    config.eyelink_flag = 0;  % 1 in production
+    config.eyelink_flag = 1;  % 1 in production
 
     % ---- scanner config ----
-    config.use_scanner_trigger = false;
+    config.use_scanner_trigger = true;
     config.trigger_key_name = 'w';
     config.TR = 2.0;
     config.dummy_seconds = 10;

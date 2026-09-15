65213192000932934ab16ad5e5749cd3e0cb6406
Yavuz Karaca <yavkaraca@gmail.com>
2026-09-16 00:23:07 +0200

    arithmetic rules fully replaced with clustered dots

--- arc_rule_generator/main.py
+++ arc_rule_generator/main.py
@@ -5,12 +5,12 @@ from stimulus import Stimulus
 from visualize import save_combined_grids
 
 from rules.arithmetic import (
-    generate_minority_takeover,
-    generate_majority_takeover,
-    generate_equalize_colors,
-    generate_increment_majority_color,
-    generate_increment_minority_color,
+    generate_majority_in_big_overtakes,
+    generate_majority_in_small_overtakes,
+    generate_minority_in_small_overtakes,
+    generate_minority_in_big_overtakes,
 )
+
 from rules.attraction import (
     generate_color_attraction,
     generate_size_attraction,
@@ -40,13 +40,33 @@ from rules.recolor import (
 
 
 RULES = {
+    "occlusion.occlusion_reversal": generate_occlusion_reversal,
+    "occlusion.mirror_x": generate_occlusion_mirror_x,
+    "occlusion.mirror_y": generate_occlusion_mirror_y,
+    "occlusion.rotate_90": generate_occlusion_rotate_90,
+    "occlusion.rotate_180": generate_occlusion_rotate_180,
 
+    "attraction.color_attraction": generate_color_attraction,
+    "attraction.size_attraction": generate_size_attraction,
+    "attraction.falling_blocks": generate_falling_blocks,
+    "attraction.floating_blocks": generate_floating_blocks,
+    "attraction.color_repulsion": generate_color_repulsion,
+
+    "expansion.star_step": generate_star_expansion_single_step,
+    "expansion.star_ray": generate_star_expansion_ray,
+    "expansion.plus_step": generate_plus_expansion_single_step,
+    "expansion.plus_ray": generate_plus_expansion_ray,
+    "expansion.3_arm_star_ray": generate_3arm_star_expansion_ray,
+
+    "arithmetic.majority_in_big_overtakes": generate_majority_in_big_overtakes,
+    "arithmetic.majority_in_small_overtakes": generate_majority_in_small_overtakes,
+    "arithmetic.minority_in_small_overtakes": generate_minority_in_small_overtakes,
+    "arithmetic.minority_in_big_overtakes": generate_minority_in_big_overtakes,
 
     "recolor.shape_color_mapping": generate_shape_color_mapping,
     "recolor.touching_edges_recolor": generate_touching_edges_recolor,
     "recolor.color_inversion": generate_color_inversion,
 }
-
 
 def main(n):
     for rule, generator in RULES.items():
